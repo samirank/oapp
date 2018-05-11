4f10a737fab42277656e0777f95892019bc4860f
samirank <samiran.kakoty@gmail.com>
2018-05-12 00:58:14 +0530

    added query.php for table creation

---
+++ queries.php
@@ -0,0 +1,5 @@
+
+<?php include("master/db.php");
+$sql = "CREATE TABLE `oapp`.`doctors` ( `doc_id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `doc_name` INT NOT NULL , `address` TEXT NOT NULL , `ph_no` INT NOT NULL , `designation` VARCHAR(50) NOT NULL , `dept` VARCHAR(50) NOT NULL , `exp` INT NOT NULL , `gender` VARCHAR(10) NOT NULL , PRIMARY KEY (`doc_id`)) ENGINE = InnoDB;";
+mysqli_query($con,$sql);
+ ?>
