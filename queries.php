f4f6036733d3ff9829513be91cb53d60c0d24746
samirank <samiran.kakoty@gmail.com>
2018-05-12 10:21:17 +0530

    fixed doctor table query

--- queries.php
+++ queries.php
@@ -1,6 +1,6 @@
 
 <?php include("master/db.php");
-$sql = "CREATE TABLE `oapp`.`doctors` ( `doc_id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `doc_name` VARCHAR(50) NOT NULL , `address` TEXT NOT NULL , `ph_no` INT NOT NULL , `designation` VARCHAR(50) NOT NULL , `dept` VARCHAR(50) NOT NULL , `exp` INT NOT NULL , `gender` VARCHAR(10) NOT NULL , PRIMARY KEY (`doc_id`)) ENGINE = InnoDB;";
+$sql = "CREATE TABLE `oapp`.`doctors` ( `doc_id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `doc_name` VARCHAR(50) NOT NULL , `address` TEXT NOT NULL , `ph_no` INT NOT NULL , `designation` VARCHAR(50) NOT NULL , `dept` INT NOT NULL , `exp` INT NOT NULL , `gender` VARCHAR(10) NOT NULL , PRIMARY KEY (`doc_id`)) ENGINE = InnoDB;";
 if (mysqli_query($con,$sql)) {
 	echo "Created table name 'doctors'";
 }else{
